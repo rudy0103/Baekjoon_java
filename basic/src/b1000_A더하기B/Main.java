a954a7d7e7fca548e201f171bd9f13612ad73a9e
박성건 <qs2720@naver.com>
2021-07-22 16:18:45 +0900

    "0722"

---
+++ basic/src/b1000_A더하기B/Main.java
@@ -0,0 +1,15 @@
+package b1000_A더하기B;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(sc.nextInt()+sc.nextInt());
+		
+
+	}
+
+}
---
+++ basic/src/b1008_A나누기B/Main.java
@@ -0,0 +1,15 @@
+package b1008_A나누기B;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println((double)(sc.nextInt())/sc.nextInt());
+		
+
+	}
+
+}
