7d941688a6afd5bc3d8a98f4135987ca61fd15f5
박성건 <qs2720@naver.com>
2021-08-04 01:29:55 +0900

    0804

--- brute_force/src/b15650_N과M_2/Main.java
+++ brute_force/src/b15650_N과M_2/Main.java
@@ -1,4 +1,4 @@
-package b15649_N과M_1;
+package b15650_N과M_2;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -12,22 +12,18 @@ public class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 	static int n,m;
-	static boolean [] isSelected;
 	static int [] arr;
 
-	static void permutation(int cnt) throws IOException {
+	static void permutation(int cnt, int next) throws IOException {
 		if(cnt==m) {
 			for(int num:arr) {
 				bw.write(num+" ");
 			}
 			bw.write("\n");
 		}else {
-			for(int i=1;i<=n;i++) {
-				if(isSelected[i]) continue;
-				isSelected[i]=true;
+			for(int i=next+1;i<=n;i++) {
 				arr[cnt]=i;
-				permutation(cnt+1);
-				isSelected[i]=false;
+				permutation(cnt+1,i);
 			}
 		}
 	}
@@ -37,12 +33,8 @@ public class Main {
 		String []inp=br.readLine().split(" ");
 		n=Integer.parseInt(inp[0]);
 		m=Integer.parseInt(inp[1]);
-		isSelected = new boolean[n+1];
 		arr = new int[m];
-		for(int i=0; i<isSelected.length;i++)
-			isSelected[i]=false;
-		
-		permutation(0);
+		permutation(0,0);
 		bw.flush();
 		bw.close();
 	}
---
+++ brute_force/src/b15651_N과M_3/Main.java
@@ -0,0 +1,48 @@
+package b15651_N과M_3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int n, m;
+	static int a = 1;
+	static int[] arr;
+
+	static void permutation(int cnt) throws IOException {
+		if (cnt == m) {
+
+			for (int num : arr) {
+				bw.write(num + " ");
+			}
+			bw.write("\n");
+		} else {
+			for (int i = 1; i <= n; i++) {
+				if (cnt == 0) {
+					arr[cnt] = i;
+					permutation(cnt + 1);
+				}
+				else if (i >= arr[cnt - 1]) {
+					arr[cnt] = i;
+					permutation(cnt + 1);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		String[] inp = br.readLine().split(" ");
+		n = Integer.parseInt(inp[0]);
+		m = Integer.parseInt(inp[1]);
+		arr = new int[m];
+
+		permutation(0);
+		bw.flush();
+		bw.close();
+	}
+}
