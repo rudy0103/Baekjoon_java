bfe791418171d901f334d47df848ce24174c10e8
박성건 <qs2720@naver.com>
2021-08-03 22:02:38 +0900

    0803

--- sort/src/b18870_좌표압축/Main.java
+++ sort/src/b18870_좌표압축/Main.java
@@ -6,9 +6,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Comparator;
 
 
 public class Main {
@@ -19,23 +18,33 @@ public class Main {
 		int n = Integer.parseInt(br.readLine());
 		String [] inp=br.readLine().split(" ");
 		
-		int [] arr= new int[n];
+		int [][] arr= new int[n][2];
 		int [] res= new int[n];
-		int [] copy;
+
 		for(int i =0; i<inp.length;i++) {
-			arr[i]=Integer.parseInt(inp[i]);
+			arr[i][0]=Integer.parseInt(inp[i]);
+			arr[i][1]=i;
 			res[i]=0;
 		}
 		
-		copy=Arrays.copyOf(arr,arr.length);
-		Arrays.sort(copy);
+		Arrays.sort(arr,new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if(o1[0]==o2[0]) return o1[1]-o2[1];
+				else return o1[0]-o2[0];
+			}
+		});
 		
-		
-		int k=0;
-		for(int i =0; i<arr.length;i++) {
-//			res[i]=copy.
+		int cnt=0;
+		res[0]=0;
+		for(int i=1; i<arr.length;i++) {
+			if(arr[i-1][0]!=arr[i][0]) cnt++;
+			res[arr[i][1]]=cnt;
 		}
-		
+		for(int num:res) {
+			bw.write(num+" ");
+		}
 		
 		bw.flush();
 		bw.close();
