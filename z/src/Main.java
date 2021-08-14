4dd1d95e6b8b795007e208af99fc11fedf79843e
박성건 <qs2720@naver.com>
2021-08-14 14:57:04 +0900

    0814

--- z/src/Main.java
+++ z/src/Main.java
@@ -1,8 +1,10 @@
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main{
 	
 	public static void main(String[] args) {
-		d
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a=1;
 	}
 }
