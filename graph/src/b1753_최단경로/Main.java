968dbd3e24ce4c6fdd0f6aff62ffe0703772f032
박성건 <qs2720@naver.com>
2021-08-24 19:17:57 +0900

    20210824

---
+++ graph/src/b1753_최단경로/Main.java
@@ -0,0 +1,82 @@
+package b1753_최단경로;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Edge{
+	
+	int vertex;
+	int weight;
+	Edge link;
+	public Edge(int vertex, int weight, Edge link) {
+		super();
+		this.vertex = vertex;
+		this.weight = weight;
+		this.link = link;
+	}
+}
+
+
+public class Main {
+	
+	public static void dijkstra(int start, Edge[] graph,int[] cost) {
+		cost[start]=0;
+		PriorityQueue<int[]> q= new PriorityQueue<>(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				 return o1[1]-o2[1];
+			}
+		});
+		q.add(new int[] {start,cost[start]});
+		
+		while(!q.isEmpty()) {
+			int []current=q.poll();
+			
+			for(Edge curEdge=graph[current[0]];curEdge!=null;curEdge=curEdge.link) {
+				int next=curEdge.vertex;
+				int nextCost=curEdge.weight;
+				int newCost=current[1]+nextCost;
+				if(cost[next]>newCost) {
+					cost[next]=newCost;
+					q.add(new int[]{next,newCost});
+				}
+			}
+		}
+	}
+	
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String [] inp = br.readLine().split(" ");
+		StringTokenizer st=null;
+		int V=Integer.parseInt(inp[0]);
+		int E=Integer.parseInt(inp[1]);
+		int start=Integer.parseInt(br.readLine());
+		
+		int [] cost=new int[V+1];
+		Arrays.fill(cost,Integer.MAX_VALUE);
+		Edge[] graph=new Edge[V+1];
+
+		for(int i=1;i<=E;i++) {
+			st=new StringTokenizer(br.readLine()," ");
+			int from=Integer.parseInt(st.nextToken());
+			int to=Integer.parseInt(st.nextToken());
+			int weight=Integer.parseInt(st.nextToken());
+			graph[from]=new Edge(to,weight,graph[from]);
+		}
+		
+		dijkstra(start,graph,cost);
+		
+		for(int i=1;i<=V;i++) {
+			if(cost[i]==Integer.MAX_VALUE) sb.append("INF\n");
+			else sb.append(cost[i]).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
--- z/src/Main.java
+++ z/src/Main.java
@@ -1,8 +1,82 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println(sc.nextInt() + sc.nextInt());
+
+	static int N; // 원소 개수
+	static int[] parents; // 부모원소관리 (트리처럼사용)
+
+	private static void make() {
+		// 모든 원소를 자신을 대표자로 만듦
+		for (int i = 1; i <= N; i++) {
+			parents[i] = i;
+		}
 	}
+
+	private static int find(int a) {
+		if (a == parents[a])
+			return a; // 자신이 대표자일 때는 자신을 리턴
+		return parents[a] = find(parents[a]); // 자신이 속한 집합의 대표쟈를 자신의 부모로 : path compression
+	}
+
+	// 두 원소를 하나의 집합으로 합치기(대표자를 이용해 합침)
+	private static void union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+
+		parents[bRoot] = aRoot; // b의 부모를 a로 삼다
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
+			sb.append("#" + t + " ");
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); // 1~N
+			int M = Integer.parseInt(st.nextToken()); // 연산
+			parents = new int[N + 1];
+
+			// 0 a b의 형태로 입력 --> a가 포함되어 있는 집합과, b가 포함되어 있는 집합을 합친다는 의미
+			// 두 원소가 같은 집합에 포함되어 있는지를 확인하는 연산은 1 a b의 형태로 입력
+			// a와 b는 n 이하의 자연수이며 같을 수도 있다.
+			// 1로 시작하는 입력에 대해서 같은 집합에 속해있다면 1을, 아니면 0을 순서대로 한줄에 연속하여 출력한다.
+
+			make(); // 원소 집합 만들기
+
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+
+				int find_union = Integer.parseInt(st.nextToken()); // 0이면 union, 1이면 find
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+
+				if (find_union == 0) {
+					union(a, b);	//a집합, b집합 합침
+				}
+				else if (find_union == 1) {
+
+					if (find(a) == find(b)) {
+						sb.append("1");		//같은 집합이면 1출력
+					} else
+						sb.append("0");	////다른 집합이면 0출력
+				}
+			}
+
+			bw.append(sb);
+			bw.flush();
+		}
+
+		bw.close();
+	}
+
 }
