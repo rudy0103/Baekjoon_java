f55c7b11f96dfed3ab33f44722472ab61dccbefc
박성건 <qs2720@naver.com>
2021-09-02 21:57:57 +0900

    20210902

--- data_structure/src/b11279_최대힙/Main.java
+++ data_structure/src/b11279_최대힙/Main.java
@@ -1,8 +1,9 @@
-package b1927_최소힙;
+package b11279_최대힙;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -10,7 +11,13 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2.compareTo(o1);
+			}
+		});
 		int N = Integer.parseInt(br.readLine());
 		
 		for(int i=0;i<N;i++) {
