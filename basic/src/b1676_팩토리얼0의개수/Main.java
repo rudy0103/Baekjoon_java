b76f1393ce2b2d6d07b2b3aa2aa9bf9c7797b5c1
박성건 <qs2720@naver.com>
2021-09-05 19:12:21 +0900

    20210905

--- basic/src/b1676_팩토리얼0의개수/Main.java
+++ basic/src/b1676_팩토리얼0의개수/Main.java
@@ -3,20 +3,20 @@ package b1676_팩토리얼0의개수;
 import java.util.Scanner;
 
 public class Main {
-	
-	static int two_cnt=0;
-	static int five_cnt=0;
-	
+
+	static int two_cnt = 0;
+	static int five_cnt = 0;
+
 	public static void checkZero(int n) {
-		
-		while(n!=1) {
-			if(n%2==0) {
-				n/=2;
+
+		while (n != 1) {
+			if (n % 2 == 0) {
+				n /= 2;
 				two_cnt++;
-			}else if(n%5==0) {
-				n/=5;
+			} else if (n % 5 == 0) {
+				n /= 5;
 				five_cnt++;
-			}else {
+			} else {
 				break;
 			}
 		}
@@ -24,14 +24,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n=sc.nextInt();
-		if(n==0) System.out.println(1);
-		else if(n<10) System.out.println(0);
+		int n = sc.nextInt();
+		if (n == 0)
+			System.out.println(0);
 		else {
-			for(int i=1;i<=n;i++) {
+
+			two_cnt = 0;
+			five_cnt = 0;
+			for (int i = 1; i <= n; i++) {
 				checkZero(i);
 			}
-			System.out.println(two_cnt<=five_cnt?two_cnt:five_cnt);
+			System.out.println(two_cnt <= five_cnt ? two_cnt : five_cnt);
 		}
 	}
 }
--- z/src/Main.java
+++ z/src/Main.java
@@ -1,64 +1,31 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Main {
 
-	static int sec = Integer.MAX_VALUE;
-	static int height = Integer.MIN_VALUE;
+	public static int cntZero(String n) {
+		int cnt = 0;
 
-	public static void getMinSecMaxHeight(int[][] map, int s,int h) {
-		if (s > sec)
-			return;
-
-		if (s == sec) {
-			if (height < h)
-				height = h;
-		} else {
-			sec = s;
-			height = h;
+		for (int i = (n.length() - 1); i >= 0; i--) {
+			if (n.charAt(i) == '0')
+				cnt++;
+			else
+				break;
 		}
-
+		return cnt;
 	}
 
-	public static void find(int[][]map,int h) {
-		int s=0;
-		
-		for(int i=0;i<map.length;i++) {
-			for(int j=0;j<map[0].length;j++) {
-				if(h-map[i][j]<0) {
-					s+=(map[i][j]-h)*2;
-				}else s+=(h-map[i][j]);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n == 0)
+			System.out.println(0);
+		else {
+			BigInteger num = new BigInteger("1");
+			for (int i = 1; i <= n; i++) {
+				num = num.multiply(new BigInteger(Integer.toString(i)));
 			}
+			System.out.println(cntZero(num.toString()));
 		}
-		getMinSecMaxHeight(map, s, h);
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		String[] inp = br.readLine().split(" ");
-		int N = Integer.parseInt(inp[0]);
-		int M = Integer.parseInt(inp[1]);
-		int B = Integer.parseInt(inp[2]);
-		int total = 0;
-		int[][] map = new int[N][M];
-		int size = N * M;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				total += map[i][j];
-			}
-		}
-		B+=total;
-		
-		for(int i=0;i<=256;i++) {
-			if(i*size<=B)
-				find(map, i);
-			else break;
-		}
-		
-		System.out.println(sec+" "+height);
 	}
 }
