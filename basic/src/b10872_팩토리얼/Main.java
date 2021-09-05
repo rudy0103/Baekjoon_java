2812d96d927fbaca7089e3045bfc063d24b56476
박성건 <qs2720@naver.com>
2021-09-05 18:16:36 +0900

    20210905

--- basic/src/b10872_팩토리얼/Main.java
+++ basic/src/b10872_팩토리얼/Main.java
@@ -3,7 +3,7 @@ package b10872_팩토리얼;
 import java.util.Scanner;
 
 public class Main {
-	public static int factorial(int n) {
+	public static long factorial(long n) {
 		
 		if(n==1 ||n==0) return 1;
 		else {
@@ -15,7 +15,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n=sc.nextInt();
+		long n=sc.nextInt();
 		
 		System.out.println(factorial(n));
 	}
---
+++ basic/src/b1676_팩토리얼0의개수/Main.java
@@ -0,0 +1,37 @@
+package b1676_팩토리얼0의개수;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	static int two_cnt=0;
+	static int five_cnt=0;
+	
+	public static void checkZero(int n) {
+		
+		while(n!=1) {
+			if(n%2==0) {
+				n/=2;
+				two_cnt++;
+			}else if(n%5==0) {
+				n/=5;
+				five_cnt++;
+			}else {
+				break;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n==0) System.out.println(1);
+		else if(n<10) System.out.println(0);
+		else {
+			for(int i=1;i<=n;i++) {
+				checkZero(i);
+			}
+			System.out.println(two_cnt<=five_cnt?two_cnt:five_cnt);
+		}
+	}
+}
