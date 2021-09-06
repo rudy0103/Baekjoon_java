bddf76b01d54a4c815d357be6b74d8c7fd420d48
박성건 <qs2720@naver.com>
2021-09-06 22:16:10 +0900

    20210906

--- graph/src/b1991_트리순회/Main.java
+++ graph/src/b1991_트리순회/Main.java
@@ -1,10 +1,79 @@
 package b1991_트리순회;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Node{
+	char val;
+	Node left;
+	Node right;
+	public Node(char val, Node left, Node right) {
+		super();
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+	
+	
+}
+
+
 public class Main {
+	
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void preorder(Node node) {
+		if(node==null) return;
+		
+		sb.append(node.val);
+		preorder(node.left);
+		preorder(node.right);
+		
+	}
+	public static void inorder(Node node) {
+		if(node==null) return;
+		
+		inorder(node.left);
+		sb.append(node.val);
+		inorder(node.right);
+		
+	}
+	public static void postorder(Node node) {
+		if(node==null) return;
+		
+		postorder(node.left);
+		postorder(node.right);
+		sb.append(node.val);
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		int N=Integer.parseInt(br.readLine());
+		Node [] tree=new Node[N];
+		for(int i=0;i<N;i++) tree[i]=new Node((char) ('A'+i),null,null);
+		
+		for(int i=0;i<N;i++) {
+			st=new StringTokenizer(br.readLine());
+			char val=st.nextToken().charAt(0);
+			char left=st.nextToken().charAt(0);
+			char right=st.nextToken().charAt(0);
+			
+			if(left!='.') tree[val-'A'].left=tree[left-'A'];
+			if(right!='.') tree[val-'A'].right=tree[right-'A'];
+		}
+		
+		preorder(tree[0]);
+		sb.append("\n");
+		inorder(tree[0]);
+		sb.append("\n");
+		postorder(tree[0]);
+		sb.append("\n");
+		System.out.println(sb.toString());
+		
+		
 	}
 
 }
---
+++ graph/src/b1991_트리순회/Main2.java
@@ -0,0 +1,69 @@
+package b1991_트리순회;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main2 {
+	static StringBuilder sb = new StringBuilder();
+	static ArrayList<ArrayList<Integer>> tree=new ArrayList<>();
+	public static void preorder(int n) {
+		if (n == -1)
+			return;
+
+		sb.append((char)(n+'A'));
+		preorder(tree.get(n).get(0));
+		preorder(tree.get(n).get(1));
+
+	}
+
+	public static void inorder(int n) {
+		if (n == -1)
+			return;
+
+		inorder(tree.get(n).get(0));
+		sb.append((char)(n+'A'));
+		inorder(tree.get(n).get(1));
+
+	}
+
+	public static void postorder(int n) {
+		if (n == -1)
+			return;
+
+		postorder(tree.get(n).get(0));
+		postorder(tree.get(n).get(1));
+		sb.append((char)(n+'A'));
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		int N=Integer.parseInt(br.readLine());
+		for(int i=0;i<N;i++) tree.add(new ArrayList<>());
+		
+		for(int i=0;i<N;i++) {
+			st=new StringTokenizer(br.readLine());
+			int val=st.nextToken().charAt(0)-'A';
+			int left=st.nextToken().charAt(0)-'A';
+			int right=st.nextToken().charAt(0)-'A';
+			
+			if(left!='.'-'A')
+				tree.get(val).add(left);
+			else tree.get(val).add(-1);
+			
+			if(right!='.'-'A')
+				tree.get(val).add(right);
+			else tree.get(val).add(-1);
+		}
+		preorder(0);
+		sb.append("\n");
+		inorder(0);
+		sb.append("\n");
+		postorder(0);
+		sb.append("\n");
+		System.out.println(sb.toString());
+	}
+}
