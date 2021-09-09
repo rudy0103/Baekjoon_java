14bf65c29aeb19bcb5367457e6aa3c3542f031ec
박성건 <qs2720@naver.com>
2021-09-09 19:04:51 +0900

    20210909

--- z/src/Main.java
+++ z/src/Main.java
@@ -1,31 +1,50 @@
-import java.math.BigInteger;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static int cntZero(String n) {
-		int cnt = 0;
-
-		for (int i = (n.length() - 1); i >= 0; i--) {
-			if (n.charAt(i) == '0')
-				cnt++;
-			else
-				break;
-		}
-		return cnt;
-	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		if (n == 0)
-			System.out.println(0);
-		else {
-			BigInteger num = new BigInteger("1");
-			for (int i = 1; i <= n; i++) {
-				num = num.multiply(new BigInteger(Integer.toString(i)));
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		int T=Integer.parseInt(br.readLine());
+		long start = System.currentTimeMillis();
+		for(int tc=0;tc<T;tc++) {
+			HashMap<String, Integer> hmap = new HashMap<>();
+			int n=Integer.parseInt(br.readLine());
+			if(n==0) {
+				sb.append("0\n");
+				continue;
 			}
-			System.out.println(cntZero(num.toString()));
+			for(int i=0;i<n;i++) {
+				st=new StringTokenizer(br.readLine());
+				String name=st.nextToken();
+				String type=st.nextToken();
+				
+				if(hmap.containsKey(type)) {
+					int tmp=hmap.get(type)+1;
+					hmap.put(type, tmp);
+				}else {
+					hmap.put(type, 1);
+				}
+			}
+			int [] cntByType=new int[hmap.size()];
+			int [] isSelected=new int[hmap.size()];
+			
+			int idx=0;
+			for(Entry<String,Integer> e:hmap.entrySet()) {
+				cntByType[idx++]=e.getValue()+1;
+			}
+			int res=1;
+			for(int i=0;i<cntByType.length;i++) {
+				res*=cntByType[i];
+			}
+			sb.append(res-1).append("\n");	
 		}
+		System.out.println(sb.toString());
 	}
 }
