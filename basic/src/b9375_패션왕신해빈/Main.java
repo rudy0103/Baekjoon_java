e681b26ec303db2128c25005e167f6de7c24ae12
박성건 <qs2720@naver.com>
2021-09-10 19:30:16 +0900

    20210910

---
+++ basic/src/b9375_패션왕신해빈/Main.java
@@ -0,0 +1,51 @@
+package b9375_패션왕신해빈;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		int T=Integer.parseInt(br.readLine());
+		long start = System.currentTimeMillis();
+		for(int tc=0;tc<T;tc++) {
+			HashMap<String, Integer> hmap = new HashMap<>();
+			int n=Integer.parseInt(br.readLine());
+			if(n==0) {
+				sb.append("0\n");
+				continue;
+			}
+			for(int i=0;i<n;i++) {
+				st=new StringTokenizer(br.readLine());
+				String name=st.nextToken();
+				String type=st.nextToken();
+				
+				if(hmap.containsKey(type)) {
+					int tmp=hmap.get(type)+1;
+					hmap.put(type, tmp);
+				}else {
+					hmap.put(type, 1);
+				}
+			}
+			int [] cntByType=new int[hmap.size()];
+			int [] isSelected=new int[hmap.size()];
+			
+			int idx=0;
+			for(Entry<String,Integer> e:hmap.entrySet()) {
+				cntByType[idx++]=e.getValue()+1;
+			}
+			int res=1;
+			for(int i=0;i<cntByType.length;i++) {
+				res*=cntByType[i];
+			}
+			sb.append(res-1).append("\n");	
+		}
+		System.out.println(sb.toString());
+	}
+}
---
+++ brute_force/src/b18111_마인크래프트/Main.java
@@ -0,0 +1,60 @@
+package b18111_마인크래프트;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int sec = Integer.MAX_VALUE;
+	static int height = Integer.MIN_VALUE;
+
+	public static void find(int[][]map,int h) {
+		int s=0;
+		
+		for(int i=0;i<map.length;i++) {
+			for(int j=0;j<map[0].length;j++) {
+				if(h-map[i][j]<0) {
+					s+=(map[i][j]-h)*2;
+				}else s+=(h-map[i][j]);
+			}
+		}
+		if(s>sec) return;
+		
+		if (s == sec) {
+			if (height < h)
+				height = h;
+		} else {
+			sec = s;
+			height = h;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		String[] inp = br.readLine().split(" ");
+		int N = Integer.parseInt(inp[0]);
+		int M = Integer.parseInt(inp[1]);
+		int B = Integer.parseInt(inp[2]);
+		int total = 0;
+		int[][] map = new int[N][M];
+		int size = N * M;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				total += map[i][j];
+			}
+		}
+		B+=total;
+		
+		for(int i=0;i<=256;i++) {
+			if(i*size<=B)
+				find(map, i);
+			else break;
+		}
+		
+		System.out.println(sec+" "+height);
+	}
+}
