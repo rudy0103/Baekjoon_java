fe4683c34bfeaa4ff41408201ead6756e7b0b059
박성건 <qs2720@naver.com>
2021-10-14 21:54:50 +0900

    20211014

--- samsungTest/src/b15684_사다리조작/Main.java
+++ samsungTest/src/b15684_사다리조작/Main.java
@@ -8,20 +8,87 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static int[][] ladder;
-	static int N,M,H;
-	
+	static int N, M, H, min = 1234;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st= new StringTokenizer(br.readLine()," ");
-		N=Integer.parseInt(st.nextToken());
-		M=Integer.parseInt(st.nextToken());
-		H=Integer.parseInt(st.nextToken());
-		
-		ladder= new int[H+2][N+1];
-		
-		for(int i=1;i<=N;i++) {
-			
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+
+		ladder = new int[H + 2][N * 2 + 1];
+
+		for (int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken()); // 행
+			int b = Integer.parseInt(st.nextToken()); // 열
+			b = b * 2 - 1;
+			ladder[a][b] = 1;
+			ladder[a][b + 1] = 1;
+			ladder[a][b + 2] = 1;
 		}
+
+		backtraking(0);
+		if (min == 1234)
+			System.out.println(-1);
+		else
+			System.out.println(min);
 	}
 
+	private static void backtraking(int d) {
+
+		int flag = 0;
+
+		for (int col = 1; col < N * 2; col += 2) {
+			if (col != goingDown(col)) {
+				flag++;
+			}
+		}
+		
+		if (flag==0) {
+			if (d < min)
+				min = d;
+			return;
+		}
+		
+		if ((3-d)*2<flag||d >= 3 || d > min)
+			return;
+
+		for (int r = 1; r <= H; r++) {
+			for (int c = 1; c < N * 2 - 1; c += 2) {
+				if (ladder[r][c] == 1)
+					continue;
+				if (ladder[r][c + 2] != 1) {
+					ladder[r][c] = 1;
+					ladder[r][c + 1] = 1;
+					ladder[r][c + 2] = 1;
+					backtraking(d + 1);
+					ladder[r][c] = 0;
+					ladder[r][c + 1] = 0;
+					ladder[r][c + 2] = 0;
+				}
+			}
+		}
+
+	}
+
+	private static int goingDown(int col) {
+		int c = col;
+		int r = 1;
+		while (r <= H) {
+			if(ladder[r][c]==0) {
+				r++;
+			}
+			else if (ladder[r][c] == 1 && ladder[r][c - 1] == 1) {
+				c -= 2;
+				r++;
+			} else if (ladder[r][c] == 1 && ladder[r][c + 1] == 1) {
+				c += 2;
+				r++;
+			} 
+		}
+
+		return c;
+	}
 }
---
+++ z/src/Main.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static int[] selected;
+	public static boolean[] visited;
+	public static int N, M;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		if (M == 1) {//
+			selected = new int[N];
+			visited = new boolean[7];
+			run1(0);
+		} else if (M == 2) {
+
+			selected = new int[N];
+			visited = new boolean[7];
+			run2(0,1);
+		} else {
+			selected = new int[N];
+			visited = new boolean[7];
+			run3(0);
+
+		}
+
+	}
+
+	private static void run1(int d) {
+		if(d==N) {
+			for (int i = 0; i < N; i++)
+				System.out.print(selected[i] + " ");
+			System.out.println();
+			return;
+		}
+		
+		for (int i = 1; i <= 6; i++) {
+			selected[d] = i;
+			run1(d + 1);
+		}
+		
+	}
+
+	private static void run2(int d,int s) {
+		if (d == N) {
+			for (int i = 0; i < N; i++)
+				System.out.print(selected[i] + " ");
+			System.out.println();
+			return;
+		}
+
+		for (int i = s; i <= 6; i++) {
+			selected[d] = i;
+			run2(d + 1,i);
+
+		}
+	}
+
+	private static void run3(int d) {
+		if (d == N) {
+			for (int i = 0; i < N; i++)
+				System.out.print(selected[i] + " ");
+			System.out.println();
+			return;
+		}
+
+		for (int i = 1; i <= 6; i++) {
+			if (visited[i])
+				continue;
+			visited[i] = true;
+			selected[d] = i;
+			run3(d + 1);
+			visited[i] = false;
+
+		}
+	}
+
+}
