d5ff6719a97efd996ef24db2288a1ca7e8ee8b70
박성건 <qs2720@naver.com>
2022-01-28 09:15:32 +0900

    20220128

--- segmentTree/src/b2243_사탕상자/Main.java
+++ segmentTree/src/b2243_사탕상자/Main.java
@@ -1,3 +1,5 @@
+package b2243_사탕상자;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,57 +7,49 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-
-		int[] tree = new int[4000004];
+		
+		int len=1000005;
+		int[] tree = new int[len*4];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 
 			String A = st.nextToken();
 			if (A.charAt(0) == '1') {// B는 꺼낼 사탕의 순위
 				int B = Integer.parseInt(st.nextToken());
-				int tmp = find(tree,B,1,1,1000000,0);
-				sb.append(tmp+"\n");
-				update(tree, tmp, -1, 1, 1, 1000000);
-
+				int f=find(tree, B, 1, 1, len, 0);
+				update(tree, f, -1, 1, 1, len);
+				sb.append(f).append("\n");
 			} else {//
-
 				int B = Integer.parseInt(st.nextToken());
 				int C = Integer.parseInt(st.nextToken());
-
-				update(tree, B, C, 1, 1, 1000000);
+				update(tree, B, C, 1, 1, len);
 			}
 
 		}
 		System.out.println(sb.toString());
 	}
 
+	private static int find(int[] tree, long b, int node, int left, int right, int cnt) {
 
-
-	private static int find(int[] tree, int b, int node, int left, int right, int cnt) {
-
-		
-		if(left==right) return left; //더이상 쪼개질 수 없을 때
-		
-		
-		int mid=(left+right)/2;
-		
-		int leftNode=node*2;
-		
-		if(b<=tree[leftNode]+cnt) {
-			return find(tree, b, node*2, left, mid, cnt);
-		}else {
-			return find(tree, b, node*2+1, mid+1, right, cnt+tree[leftNode]);
+		if (left == right) {
+			return  left;
 		}
-		
-		
+
+		int leftVal = tree[node * 2];
+		int mid = (left + right) / 2;
+		if (cnt + leftVal < b) {
+			return find(tree, b, node * 2 + 1, mid + 1, right, leftVal + cnt);
+		} else {
+			return find(tree, b, node * 2, left, mid, cnt);
+		}
+
 	}
-
-
 
 	private static void update(int[] tree, int index, int val, int node, int left, int right) {
 
@@ -64,7 +58,7 @@ public class Main {
 
 		if (left == right) {
 			tree[node] += val;
-			return ;
+			return;
 		}
 
 		int mid = (left + right) / 2;
@@ -76,7 +70,5 @@ public class Main {
 		}
 
 		tree[node] = tree[node * 2] + tree[node * 2 + 1];
-
 	}
-
 }
