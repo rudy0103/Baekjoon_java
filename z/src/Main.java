04771acdc08d99c1c10f0bdf3c6a596d7b1877b2
박성건 <qs2720@naver.com>
2022-02-04 23:48:23 +0900

    20220204

--- z/src/Main.java
+++ z/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
 public class Main {
 
@@ -12,13 +13,26 @@ public class Main {
 		
 		String inp=br.readLine();
 		
+		HashSet<Character> set = new HashSet<>();
+		set.add('a');
+		set.add('i');
+		set.add('u');
+		set.add('e');
+		set.add('o');
 		
+		set.add('A');
+		set.add('I');
+		set.add('U');
+		set.add('E');
+		set.add('O');
 		while(!inp.equals("#")) {
-			
-			
-			
-			
-			
+			int cnt=0;
+			for(int i=0;i<inp.length();i++) {
+				if(set.contains(inp.charAt(i))) {
+					cnt++;
+				}
+			}
+			System.out.println(cnt);
 			inp=br.readLine();
 		}
 		
