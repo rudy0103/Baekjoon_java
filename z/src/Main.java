92942e07502489f10a830c4b4e20e7c0e32fafee
rudy0103 <qs2720@naver.com>
2022-02-09 23:31:56 +0900

    20220209

--- z/src/Main.java
+++ z/src/Main.java
@@ -1,43 +1,93 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
-	static long cnt=0;
-	static int[] newArr;
+
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+		int N=10;
+		int [] arr = new int[N];
 		
-		String inp=br.readLine();
+		for(int i=0;i<N;i++) arr[i]=i;
 		
-		HashSet<Character> set = new HashSet<>();
-		set.add('a');
-		set.add('i');
-		set.add('u');
-		set.add('e');
-		set.add('o');
 		
-		set.add('A');
-		set.add('I');
-		set.add('U');
-		set.add('E');
-		set.add('O');
-		while(!inp.equals("#")) {
-			int cnt=0;
-			for(int i=0;i<inp.length();i++) {
-				if(set.contains(inp.charAt(i))) {
-					cnt++;
-				}
-			}
-			System.out.println(cnt);
-			inp=br.readLine();
+		ArrayList<int[]> list = getPair(arr,10,10);
+		
+		System.out.println(list.size());
+		
+		for(int[] e:list) {
+			System.out.println(e[0]+" "+e[1]);
 		}
 		
 		
 	}
+
+	private static ArrayList<int[]> getPair(int[] arr, int size, int sum) {
+		
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+		
+		ArrayList<int[]> list =new ArrayList<>();
+		
+		for(int pos=0;pos<size-1;pos++) {
+			
+			func(pos,size-1,sum,arr,list);
+			
+		}
+		
+		return list;
+	}
+
+	private static void func(int pos, int r, int sum,int[] arr,ArrayList<int[]> list) {
+		
+		
+		int left=pos+1;
+		int right=r;
+		
+		int mid=(left+right)/2;
+		
+		while(left<=right) {
+			
+			int e=arr[mid];
+			
+			if(e+arr[pos]==sum) {
+				
+				list.add(new int[] {arr[pos],e});
+				
+				for(int p=mid-1;p>pos;p--) {
+					if(arr[p]!=e) break;
+					list.add(new int[] {arr[p],e});
+				}
+				
+				for(int p=mid+1;p<mid;p++) {
+					if(arr[p]!=e) break;
+					list.add(new int[] {arr[p],e});
+				}
+				break;
+				
+			}else if(e+arr[pos]<sum) {
+				
+				left=mid+1;
+				mid=(left+right)/2;
+				
+			}else {
+				
+				right=mid-1;
+				mid=(left+right)/2;
+				
+			}
+			
+		}
+		
+		
+		
+	}
+	
+	
 
 
 
